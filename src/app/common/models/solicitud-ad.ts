fc920810169215cf2db8a4bae889317656119815
Ruben Cancino <rubencancino6@gmail.com>
2019-04-28 08:16:06 -0500

    Solicitud-ad model

---
+++ src/app/common/models/solicitud-ad.ts
@@ -0,0 +1,32 @@
+/**
+ * Solicitud para autorizacion de deposito
+ * 
+ */
+export class SolicitudAd {
+  id: string;
+  nombre: string;
+  sucursal: string;
+  sucursalNombre: string;
+  cliente: string;
+  banco: string;
+  cuenta: string;
+  cobro?: any;
+  tipo: string;
+  folio: number;
+  fecha: string;
+  fechaDeposito: string;
+  referencia: string;
+  cheque: number;
+  efectivo: number;
+  transferencia: number;
+  total: number;
+  comentario: string;
+  cancelacion?: string;
+  cancelacionComentario?: string;
+  enviado: boolean;
+  dateCreated?: string;
+  lastUpdated?: string;
+  createUser?: string;
+  updateUser: string;
+  status: string;
+}
